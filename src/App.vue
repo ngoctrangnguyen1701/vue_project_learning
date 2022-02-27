1624463ed68f70178a22047ec5e5ac699eb9ea32
ngoctrangnguyen1701 <89184893+ngoctrangnguyen1701@users.noreply.github.com>
2022-02-27 20:15:56 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import MyComponent from './components/MyComponent.vue'
+
+</script>
+
+<template>
+  <MyComponent message="Hello Vietnam"/>
+  <MyComponent message="Xin chao Viet Nam"/>
+  <MyComponent message="My Component"/>
+</template>
+
+<style>
+  @import './assets/base.css';
+  h1{
+    color: white;
+  }
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+defineProps({
+  message: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<script>
+  export default {
+    data: function(){
+      return {
+        test: 'white',
+        isRed: false
+      }
+    },
+    methods: {
+      changeColor: function() {
+        this.isRed = !this.isRed
+        this.test = this.isRed === true ? 'red' : 'white'
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1 :style="{color: test}">{{message}}</h1>
+  <button @click="changeColor">color: {{test}}</button>
+</template>
