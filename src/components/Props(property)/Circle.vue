059ae52dfb478446d396c937db4f352db332d5c8
unknown <ngoctrangnguyen1701@gmail.com>
2022-03-02 14:12:21 +0700

    props continue

---
+++ src/components/Props(property)/Circle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="circle"></div>
+</template>
+
+<script>
+export default {
+  props: [
+    
+  ]
+}
+</script>
---
+++ src/components/Props(property)/Props.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Props(property)</h1>
+  <button>Turn</button>
+</template>
